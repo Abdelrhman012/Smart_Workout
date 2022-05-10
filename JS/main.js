7a2dce100703cea5f0bbe59e503e79795977be4f
Abdelrhman012 <you@example.com>
2022-05-10 08:23:43 +0200

    V1.1

--- JS/main.js
+++ JS/main.js
@@ -54,94 +54,33 @@ document.addEventListener("click", function (e) {
     }
     
 });
-let gymFreeExcercise = document.querySelectorAll(".free-excercise")
-let gymFreeExcercises = Array.from(gymFreeExcercise)
 
-let startExcersize = document.createElement("div")
-let excerciseStart = document.createElement("div");
-let backExcersize = document.createElement("div")
-let subHeader = document.querySelector(".sp-subheader")
-let timer = document.createElement("div")
-let timerBox = document.createElement("div")
-let timerBoxText = document.createElement("div")
-let stopExcersize = document.createElement("div")
-
-console.log(subHeader)
-
-let exit = document.querySelector("backExcersize-btn")
-gymFreeExcercise.forEach((ele) => 
-
-    document.addEventListener("click", function (e) {
-    
-        if (e.target.classList.contains("backExcersize-btn")) {
-            ele.classList.remove("hide")
-            ele.classList.remove("display-card")
-            let excercise = document.querySelector(".display-card .gymFreeModeCard-Text")
-            excercise.removeChild(excerciseStart)
-            excercise.removeChild(timerBox)
-            excercise.removeChild(stopExcersize)   
-            subHeader.style.display = "block"
-        }
+let displayCards = document.querySelector(".display-cards")
+let displayCard = displayCards.querySelectorAll(".display-card")
+document.querySelectorAll(".cards .card").forEach(card => {
+    card.onclick = () => {
+        displayCards.style.display = " block"
+        let name = card.getAttribute("data-exercise")
+        displayCard.forEach(display => {
+            let details = display.getAttribute("data-card")
+            console.log(display)
+            if (name === details) {
+                display.classList.add('active');
+            }
+        })
     }
-    )
-        
-    
-        
-    
-    )
+})
 
-gymFreeExcercises.forEach((ele) => {   
-    ele.addEventListener("click", function (e) {
-        if (e.currentTarget.classList.contains("hide")) {
-            e.currentTarget.classList.remove("hide");
-        } else {
-            gymFreeExcercises.forEach((ele) => {
-                ele.classList.add("hide");
-            });
-            e.currentTarget.classList.remove("hide");;
-            e.currentTarget.classList.add("display-card");
-            subHeader.style.display = "none"
-        }
-        if (e.currentTarget.classList.contains("display-card")) {
-            startExcersize.style.display = "block"
-            backExcersize.style.display = "block"
-            let excercise = document.querySelector(".display-card .gymFreeModeCard-Text")
-            excerciseStart.className = "excercise-card"
-            startExcersize.innerHTML = "Start"
-            startExcersize.className = "button startExcersize-btn"
-            backExcersize.innerHTML = "Back"
-            backExcersize.className = "button stop-exercise backExcersize-btn"
-            excerciseStart.appendChild(startExcersize)
-            excerciseStart.appendChild(backExcersize)
-            excercise.appendChild(excerciseStart)
-        }
-    })
+displayCard.forEach(close => {
+    close.querySelector('.backExcersize-btn').onclick = () =>{
+    close.classList.remove('active');
+    displayCards.style.display = 'none';
+    };
+})
 
-    ele.addEventListener("click", function (e) {
-        if (e.target.classList.contains("startExcersize-btn")) {
-            startExcersize.style.display = "none"
-            backExcersize.style.display = "none"
-            timer.innerHTML = "10"
-            timer.className = "timer exercise-timer"
-            timerBox.className = "timer-box"
-            timerBoxText.innerHTML= "Will Start in"
-            timerBoxText.className = "timerboxtext"
-            timerBox.appendChild(timerBoxText)
-            timerBox.appendChild(timer)
-            stopExcersize.className = "button stop-exercise backExcersize-btn"
-            stopExcersize.innerHTML = "Stop"
-            let excercise = document.querySelector(".display-card .gymFreeModeCard-Text")
-            excercise.appendChild(timerBox)
-            excercise.appendChild(stopExcersize)
-            function countdown() {
-            timer.innerHTML -= 1;
-            if (timer.innerHTML === "0") {
-                clearInterval(counter);
-                timerBox.style.display = "none"
-
-            }
-            }
-            let counter = setInterval(countdown, 1000);
-        }
-    })
+displayCard.forEach(start => {
+    start.querySelector('.startExcersize-btn').onclick = () =>{
+    start.classList.remove('active');
+    displayCards.style.display = 'none';
+    };
 })
